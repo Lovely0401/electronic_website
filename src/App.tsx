419cd23d12e4ccb25d5d78ed2840cd722a00f6b5
Lovely <lovelysingh012002@gmail.com>
2026-03-19 11:41:01 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App(){
+    return (
+        <div>
+            <h1 className="text-3x1 font-bold text-center mt-10 text-blue-600">
+                Electronic store Setup Ready!
+
+            </h1>
+        </div>
+    )
+}
